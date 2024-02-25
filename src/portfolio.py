b17f1ec1aa0ad3fc6e9b1b0d2ff98b045f7010ee
Trin <>
2024-02-25 22:42:31 +0700

    in progress

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,31 +1,9 @@
 import numpy as np
+import pandas as pd
 
 class Portfolio:
-
     @classmethod
-    def compute_portfolio(cls, weights, prices, initial_capital=1000000):
-        '''
-        Compute rebalanced allocations for each assset on each period based on weights and prices. 
-        Rebalancing is calculated based on portfolio value (price x previous allocations) then compute a new allocation
-        based on given weights.
-
-        Returns - allocations (num_shares), portfolio_value for each period.
-        '''
-        assert weights.shape == prices.shape, 'Shape of weights and prices must be the same'
-        allocations = np.zeros(shape=weights.shape) # additional column for cash
-        current_value = initial_capital
-
-        for i in range(weights.shape[0]):
-            current_value = current_value or (allocations[i-1,:] * prices[i,:]).sum(axis=0)
-            value_alloc = current_value * weights[i,:]
-            share_alloc = value_alloc / prices[i,:]
-            allocations[i] = share_alloc
-            print('CURRENT', current_value)
-            print('  weight', weights[i,:])
-            print('  current_value', (allocations[i-1,:] * prices[i,:]))
-            print('  value_alloc', value_alloc)
-            print('  portfolio', allocations[i])
-            print('  price', prices[i])
-            current_value = None
-        
-        return allocations, (allocations * prices).sum(axis=0)
+    def portfolio_returns(cls, weights, prices):
+        weights = pd.DataFrame(weights)
+        returns = pd.DataFrame(prices).pct_change().shift(-1)
+        return weights.multiply(returns).sum(axis=1)
--- src/train.py
+++ src/train.py
@@ -11,13 +11,11 @@ def main(num_epochs=1, batch_size=32, learning_rate=0.001, shuffle=False):
     yfdata, features, targets, X, y, y_price = load_train_test()
     
     print('========= Original Dataset =========')
-    print('features', features.shape, 'columns', features.columns, features.index[0], features.index[-1])
-    print('targets', targets.shape, 'columns', targets.columns, targets.index[0], targets.index[-1])
+    print('features', features.shape, 'columns', features.index[0], features.index[-1])
+    print('targets', targets.shape, 'columns', targets.index[0], targets.index[-1])
     print('X', X.shape)
     print('y', y.shape)
     print('y_price', y_price.shape)
-
-    print(y)
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     
@@ -29,21 +27,28 @@ def main(num_epochs=1, batch_size=32, learning_rate=0.001, shuffle=False):
     print('y_price', y_price.shape)
 
     # TODO: fix model so loss are reduced
-    # model = train(X_train, y_train, num_epochs, batch_size, learning_rate, shuffle)
+    model = train(X_train, y_train, num_epochs, batch_size, learning_rate, shuffle)
 
     # # TODO: show Portfolio value from train dataset
-    optimal_test_portfolio = Portfolio.compute_portfolio(y, y_price)
-    # predicted_test_portfolio = Portfolio.compute_portfolio(model.predict(X_test), )
+    optimal_test_portfolio = Portfolio.portfolio_returns(y, y_price)
+    print(optimal_test_portfolio)
+    print((optimal_test_portfolio+1).cumprod())
+    predicted_test_portfolio = Portfolio.portfolio_returns(model.predict(X_test), y_price)
+
+    print('----------- predicted -----------')
+    print(predicted_test_portfolio)
+    print((predicted_test_portfolio+1).cumprod())
 
     # TODO: show Portfolio value from test dataset
 
 
 def train(X_train, y_train, num_epochs, batch_size, learning_rate, shuffle):
+
     model = Sequential()
     model.add(LSTM(units=X_train.shape[2]*4, input_shape=(X_train.shape[1], X_train.shape[2])))
     model.add(Dense(units=y_train.shape[1]))  # output for optimal weights for next week.
 
-    model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
     model.fit(X_train, y_train, epochs=num_epochs, batch_size=batch_size, validation_split=0.2, verbose=1)
     return model
 
